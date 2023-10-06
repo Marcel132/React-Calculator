664cbb645fd0a0a1a033c78ee3f6ff2fdb23f587
Marcel Podsiadło <marcelpodsiadlo.business@gmail.com>
2023-10-06 09:12:41 +0200

    Update calculator.jsx
    Change class -> className

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -33,27 +33,27 @@ export default function Calc() {
   }
 
     return(
-        <div class="container">
-            <div class="board" id="calc_board">
-                <div id="screen" class="border">{result ? result : expression}</div>
+        <div className="container">
+            <div className="board" id="calc_board">
+                <div  className="border" id="screen">{result ? result : expression}</div>
                 <div id="buttons_board">
-                    <button class="buttons" onClick={() => handleButtonClick("7")}>7</button>
-                    <button class="buttons" onClick={() => handleButtonClick("8")}>8</button>
-                    <button class="buttons" onClick={() => handleButtonClick("9")}>9</button>
-                    <button class="buttons" onClick={() => handleButtonClick("*")}>*</button>
-                    <button class="buttons" onClick={() => handleButtonClick("4")}>4</button>
-                    <button class="buttons" onClick={() => handleButtonClick("5")}>5</button>
-                    <button class="buttons" onClick={() => handleButtonClick("6")}>6</button>
-                    <button class="buttons" onClick={() => handleButtonClick("/")}>/</button>
-                    <button class="buttons" onClick={() => handleButtonClick("1")}>1</button>
-                    <button class="buttons" onClick={() => handleButtonClick("2")}>2</button>
-                    <button class="buttons" onClick={() => handleButtonClick("3")}>3</button>
-                    <button class="buttons" onClick={() => handleButtonClick("-")}>-</button>
-                    <button class="buttons" id="clear" onClick={() => handleButtonClick("C")}>C</button>
-                    <button class="buttons" id="equal" onClick={() => handleButtonClick("0")}>0</button>
-                    <button class="buttons" onClick={() => handleButtonClick("=")}>=</button>
-                    <button class="buttons" onClick={() => handleButtonClick("+")}>+</button>
-                    <button class="buttons" onClick={() => handleButtonClick(".")}>.</button>
+                    <button className="buttons" onClick={() => handleButtonClick("7")}>7</button>
+                    <button className="buttons" onClick={() => handleButtonClick("8")}>8</button>
+                    <button className="buttons" onClick={() => handleButtonClick("9")}>9</button>
+                    <button className="buttons" onClick={() => handleButtonClick("*")}>*</button>
+                    <button className="buttons" onClick={() => handleButtonClick("4")}>4</button>
+                    <button className="buttons" onClick={() => handleButtonClick("5")}>5</button>
+                    <button className="buttons" onClick={() => handleButtonClick("6")}>6</button>
+                    <button className="buttons" onClick={() => handleButtonClick("/")}>/</button>
+                    <button className="buttons" onClick={() => handleButtonClick("1")}>1</button>
+                    <button className="buttons" onClick={() => handleButtonClick("2")}>2</button>
+                    <button className="buttons" onClick={() => handleButtonClick("3")}>3</button>
+                    <button className="buttons" onClick={() => handleButtonClick("-")}>-</button>
+                    <button className="buttons" id="clear" onClick={() => handleButtonClick("C")}>C</button>
+                    <button className="buttons" id="equal" onClick={() => handleButtonClick("0")}>0</button>
+                    <button className="buttons" onClick={() => handleButtonClick("=")}>=</button>
+                    <button className="buttons" onClick={() => handleButtonClick("+")}>+</button>
+                    <button className="buttons" onClick={() => handleButtonClick(".")}>.</button>
                     <Link to="/"><button class="buttons" id="exit_button">Powrót</button></Link> {/* Return to main page (main.jsx)*/}
                 </div>
             </div>
