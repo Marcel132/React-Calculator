896acb3bff44355211dbf485e730cea55e427f64
Marcel Podsiadło <marcelpodsiadlo.business@gmail.com>
2023-10-06 09:14:42 +0200

    Update main.jsx
    Change class -> className

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,11 +5,11 @@ import './css/main.css'
 
 export default function Home() {
     return (
-        <div class="container flex">
+        <div className="container flex">
             <h1 id="header">Witaj w przeglądarkowym kalkulatorze!</h1>
-            <div id="button_box" class="flex">
-                <Link to="/calculator"><button class="button_list">Kalkulator</button></Link> {/* Go to calculator (calculator.jsx) */}
-                <Link to="/hail"><button class="button_list">Miary</button></Link> {/* Go to hail calculator (hail.jsx */}
+            <div id="button_box" className="flex">
+                <Link to="/calculator"><button className="button_list">Kalkulator</button></Link> {/* Go to calculator (calculator.jsx) */}
+                <Link to="/hail"><button className="button_list">Miary</button></Link> {/* Go to hail calculator (hail.jsx */}
             </div>
         </div>
     )
